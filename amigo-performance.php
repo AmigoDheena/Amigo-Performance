8bcec51387bab950cbcea5428602cbb8f8557e8c
AmigoDheena <amigodheena@gmail.com>
2020-07-01 20:07:16 +0530

    perf

--- amigo-performance.php
+++ amigo-performance.php
@@ -37,18 +37,35 @@ class AmigoPerformancePlugin{
             __( 'Amigo Performance', 'textdomain' ),
             'AmigoPerf',
             'manage_options',
-            plugins_url('amigo-performance/amigoPerformance-admin.php'),
+            plugins_url('amigoPerformance-admin.php', __FILE__),
             '',
             'dashicons-buddicons-activity',
-            // plugins_url( 'amigo-performance/images/icon3.png' ),
+            // plugins_url( 'amigo-performance/assets/imag/icon3.png' ),
             6
         );
+    }
+
+    // Enqueue Style sheets
+    function amigoperformance_enqueue_style(){
+        wp_enqueue_style('amigoperf_style', plugins_url('assets/css/style.css',__FILE__));
+    }
+    
+    // Enqueue Scripts
+    function amigoperformance_enqueue_script(){
+        wp_enqueue_script('amigoperf_script', plugins_url('assets/js/script.js',__FILE__));
+    }
+
+    // Register Style sheets and Scripts
+    function amigoperformance_register(){
+        add_action('admin_enqueue_scripts', array($this , 'amigoperformance_enqueue_style'));
+        add_action('admin_enqueue_scripts', array($this , 'amigoperformance_enqueue_script'));
     }
 
 }
 
 if (class_exists('AmigoPerformancePlugin')) {
     $amigoperformanceplugin = new AmigoPerformancePlugin();
+    $amigoperformanceplugin -> amigoperformance_register();
 }
 
 // Activation
