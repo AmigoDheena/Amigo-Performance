a5c077368e0081f71b8e93879a40eeb7e1628912
AmigoDheena <amigodheena@gmail.com>
2020-07-04 14:38:48 +0530

    rmemoji
    Remove Emoji section completed

--- amigo-performance-admin.php
+++ amigo-performance-admin.php
@@ -15,17 +15,26 @@
         }
 
         public function amigoPerf_Default(){
-            global $amigoPerf_rqs_opt;
+            global $amigoPerf_rqs_opt, $amigoPerf_remoji_opt;
+
             $this->amigoPerf_rqs_opt = ( FALSE !== get_option($this->amigoPerf_rqs) ? get_option($this->amigoPerf_rqs) : 'on'  ); 
+            $this->amigoPerf_remoji_opt = ( FALSE !== get_option($this->amigoPerf_remoji) ? get_option($this->amigoPerf_remoji) : 'on'  ); 
+
             $this->amigoPerf_rqs = 'amigoPerf_rqs';
+            $this->amigoPerf_remoji = 'amigoPerf_remoji';
+
             $this->amigoPerf_rqs_val = $amigoPerf_rqs_opt;
+            $this->amigoPerf_remoji_val = $amigoPerf_remoji_opt;
         }
             
         public function amigoperf_hiddenField(){
             if (isset($_POST[$this->amigoPerf_hfn]) && $_POST[$this->amigoPerf_hfn] == 'Y') {
                 $this->amigoPerf_rqs_val = (isset($_POST[$this->amigoPerf_rqs]) ? $_POST[$this->amigoPerf_rqs] : "off");
+                $this->amigoPerf_remoji_val = (isset($_POST[$this->amigoPerf_remoji]) ? $_POST[$this->amigoPerf_remoji] : "off");
 
                 update_option( $this->amigoPerf_rqs, $this->amigoPerf_rqs_val );
+                update_option( $this->amigoPerf_remoji, $this->amigoPerf_remoji_val );
+
                 flush_rewrite_rules();
             }
         }
@@ -49,24 +58,41 @@
             
         }
 
+        public function amigoPerf_remoji_operation()
+        {
+            if($this->amigoPerf_remoji_opt == get_option($this->amigoPerf_remoji)) {
+                remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
+                remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); 
+                remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
+                remove_action( 'admin_print_styles', 'print_emoji_styles' );
+            }
+        }
+
         public function amigoPerf_menu(){ ?>
             <div class='amperf-container'>
 
                 <div class= 'amperf-header'>
-                    <h1> <?php echo $this->PluginName?> </h1>
+                    <h1> <?php echo $this->PluginName?><span> <?php echo $this->PluginVersion ?> </span> </h1>
+                    
                 </div>
 
                 <form method="post" id="formid">
                     <input type="hidden" name="<?php echo $this->amigoPerf_hfn; ?>" value="Y">
+
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" name="<?php echo $this->amigoPerf_rqs; ?>" value="<?php echo $this->amigoPerf_rqs_opt ?>" <?php if($this->amigoPerf_rqs_opt == get_option($this->amigoPerf_rqs)) echo 'checked="checked"'; ?> <?php checked($this->amigoPerf_rqs_val, 'on',true) ?> >
                         <label class="custom-control-label" for="<?php echo $this->amigoPerf_rqs; ?>" <?php esc_attr_e('Remove query strings from static content', 'Amigo-Performance'); ?>>Remove Query Strings</label>
                     </div>
+
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" name="<?php echo $this->amigoPerf_remoji; ?>" value="<?php echo $this->amigoPerf_remoji_opt ?>" <?php if($this->amigoPerf_remoji_opt == get_option($this->amigoPerf_remoji)) echo 'checked="checked"'; ?> <?php checked($this->amigoPerf_remoji_val, 'on',true) ?> >
+                        <label class="custom-control-label" for="<?php echo $this->amigoPerf_remoji; ?>" <?php esc_attr_e('Remove query strings from static content', 'Amigo-Performance'); ?>>Remove Emoji</label>
+                    </div>
+
                     <input type="submit" value="<?php esc_attr_e('Save Changes','Amigo-Performance') ?>" class="amperf-submitbtn" name="submit">
                 </form>
 
             </div>
-
        <?php } //amigoPerf_menu() End
 
        // Register Menu Page
@@ -86,5 +112,6 @@
     $amigoPerfDefault ->amigoPerf_Default();
     $amigoPerfDefault -> amigoperf_hiddenField();
     $amigoPerfDefault -> amigoPerf_rqs_query('details');
-    $amigoPerfDefault -> amigoPerf_rqs_operation();
+    $amigoPerfDefault -> amigoPerf_rqs_operation(); //Remove Query Strings Operation
+    $amigoPerfDefault -> amigoPerf_remoji_operation(); //Remove Emoji Operation
 ?>
