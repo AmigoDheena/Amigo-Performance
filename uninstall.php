5abd47fadad21bf1eb12ae94387f30e407629010
AmigoDheena <amigodheena@gmail.com>
2020-07-13 14:38:26 +0530

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,6 +2,9 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
   die;
 }
+
+if (!get_option('plugin_do_uninstall', false)) exit;
+
 delete_option( 'amigoPerf_rqs' );
 delete_option( 'amigoPerf_remoji' );
 delete_option( 'amigoPerf_defer' );
