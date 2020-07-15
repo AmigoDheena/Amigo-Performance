a02c7ad813ef7b059721c6ec63d230b8fbb8c425
AmigoDheena <amigodheena@gmail.com>
2020-07-15 15:52:38 +0530

    licence
    licence updated

--- amigo-performance.php
+++ amigo-performance.php
@@ -11,6 +11,8 @@
  * Author:            Amigo Dheena
  * Author URI:        https://www.amigodheena.xyz
  * Text Domain:       amigo-performance
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 if (!defined('ABSPATH')) {
