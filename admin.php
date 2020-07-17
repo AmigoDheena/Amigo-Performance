783918721145b8b2122d77e7c8fb217dc49e3796
AmigoDheena <amigodheena@gmail.com>
2020-07-17 23:06:01 +0530

    checked();
    checked() used insted of if condition

--- admin.php
+++ admin.php
@@ -17,22 +17,22 @@
         <input type="hidden" name="<?php esc_html_e($this->amigoPerf_hfn,'amigo-peformance'); ?>" value="Y">
 
         <label class="amigoPerf_lable"><?php esc_html_e('Remove Query String','amigo-performance'); ?>
-            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_rqs,'amigo-peformance'); ?>" value="<?php  esc_html_e($this->amigoPerf_rqs_opt,'amigo-peformance'); ?>" <?php if($this->amigoPerf_rqs_opt == get_option($this->amigoPerf_rqs)) esc_html_e('checked="checked"','amigo-peformance'); ?>  >
+            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_rqs,'amigo-peformance'); ?>" value="<?php  esc_html_e($this->amigoPerf_rqs_opt,'amigo-peformance'); ?>" <?php checked($this->amigoPerf_rqs_opt == get_option($this->amigoPerf_rqs),true);?>  >
             <span class="checkmark"></span>
         </label>
 
         <label class="amigoPerf_lable"><?php esc_html_e('Remove Emoji','amigo-performance'); ?>
-            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_remoji,'amigo-peformance'); ?>" value="<?php esc_html_e($this->amigoPerf_remoji_opt,'amigo-peformance'); ?>" <?php if($this->amigoPerf_remoji_opt == get_option($this->amigoPerf_remoji)) esc_html_e('checked="checked"','amigo-peformance'); ?>  >
+            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_remoji,'amigo-peformance'); ?>" value="<?php esc_html_e($this->amigoPerf_remoji_opt,'amigo-peformance'); ?>" <?php checked($this->amigoPerf_remoji_opt == get_option($this->amigoPerf_remoji),true);?>  >
             <span class="checkmark"></span>
         </label>
 
         <label class="amigoPerf_lable"><?php esc_html_e('Defer parsing of JavaScript','amigo-performance'); ?>
-            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_defer,'amigo-peformance'); ?>" value="<?php esc_html_e($this->amigoPerf_defer_opt,'amigo-peformance'); ?>" <?php if($this->amigoPerf_defer_opt == get_option($this->amigoPerf_defer)) esc_html_e('checked="checked"','amigo-peformance'); ?>  >
+            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_defer,'amigo-peformance'); ?>" value="<?php esc_html_e($this->amigoPerf_defer_opt,'amigo-peformance'); ?>" <?php checked($this->amigoPerf_defer_opt == get_option($this->amigoPerf_defer),true);?>  >
             <span class="checkmark"></span>
         </label>
 
         <label class="amigoPerf_lable"><?php esc_html_e('Iframe Lazyload','amigo-performance'); ?>
-            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_iframelazy,'amigo-peformance'); ?>" value="<?php esc_html_e($this->amigoPerf_iframelazy_opt,'amigo-peformance'); ?>" <?php if($this->amigoPerf_iframelazy_opt == get_option($this->amigoPerf_iframelazy)) esc_html_e('checked="checked"','amigo-peformance'); ?>  >
+            <input type="checkbox" class="custom-control-input" name="<?php esc_html_e($this->amigoPerf_iframelazy,'amigo-peformance'); ?>" value="<?php esc_html_e($this->amigoPerf_iframelazy_opt,'amigo-peformance'); ?>" <?php checked($this->amigoPerf_iframelazy_opt == get_option($this->amigoPerf_iframelazy), true); ?>  >
             <span class="checkmark"></span>
         </label>
         
